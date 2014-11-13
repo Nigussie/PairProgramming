cf7ae9219a2fe0cc2e7b90b7ce57e0c7797a3e81
Nigussie <nmezgebe1@gmail.com>
2014-11-13 16:54:07 +0200

    Modified the script to add computers

--- Problem_E/Tournament.m
+++ Problem_E/Tournament.m
@@ -1,12 +1,22 @@
 clear all; clc;
 stock ={'r','p','s','R', 'P','S'};
- str = input('Enter a letter to compare:','s');
-for i= 1:length(stock)
-   if(strcmp(str,char(stock{i})))
-        disp('legal move')
-   else        
-        disp('tournament over')
-        break;
-    end
+mymove = input('Enter a letter to compare:','s');
+ran =randi([ 1 3]);
+computersmove = stock{ran};
+
+   
+%    if(mymove==computersmove)
+%         disp('legal move')
+
+        if(mymove==computersmove)
+            dip('Draw')
+        elseif(((mymove == 'r') && (computersmove =='s')) || ((mymove == 'p') && (computersmove =='r')) || ((mymove == 's') && (computersmove =='p')) )
+            disp('Sorry, but I lost: Comp') % Computer loses
+        else
+            disp('You got served! user:') %Player loses
+        end           
+%         disp('tournament over')
+%         break;
+%     end
     
-end
+
